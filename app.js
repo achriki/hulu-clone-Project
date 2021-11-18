ea4b9afed8af8188963ab360edec22045774bd62
mac <mac@MacBook-Pro-de-mac.local>
2021-11-18 19:15:20 +0000

    adding channels area form

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // variables
 const btnLogin = document.querySelector(".btn-login")
-const Model = document.querySelector(".loginModal")
+const Model = document.querySelector(".Modal")
 const btnClose = document.querySelector(".btn-close")
+const btnArea = document.querySelector(".btn-check-area")
+const channelModel = document.querySelector(".channel-modal")
 //Events
 btnLogin.addEventListener("click",function(){
     Model.style.display ="block"    
@@ -9,8 +11,14 @@ btnLogin.addEventListener("click",function(){
 btnClose.addEventListener("click",function(){
     Model.style.display = "none"
 })
+btnArea.addEventListener("click",function(){
+    channelModel.style.display = "block"
+})
 window.addEventListener("click",function(e){
     if(e.target === Model){
         Model.style.display = "none"
+    }
+    else if(e.target === channelModel){
+        channelModel.style.display = "none"
     }
 })
